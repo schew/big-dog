4fea8c5fd50a590a73db315718ec6c8206b50194
ikuznet1 <ivan.kuznetsov1@gmail.com>
2015-09-13 00:01:16 -0400

    R upd
    yay

--- functions/App.R
+++ functions/App.R
@@ -2,8 +2,143 @@ library(shiny)
 library(ggplot2)
 library(robustbase)
 library(reshape)
+library(xlsx)
+
+DataScrubbing <- function(file_name) 
+{
+  # This function takes an excel spreadsheet with the first row as labels. It will cut off all rows beyond 100  
+  # for the columns with both continuous/Discrete and categorical data and returns 
+  # a data set with the first column as the header 
+  
+  file_name <- paste(file_name,".xlsx",sep="") # add excel extenstion 
+  library(xlsx) # import excel library for reading 
+  df <- read.xlsx(file_name,1,header=FALSE,stringsAsFactors = FALSE)
+  columns <- df[1,]; # store the column names for future reference 
+  df <- df[2:dim(df)[1],] # remove the first row 
+  row_names <- df[,1]; 
+  names(df) <- columns 
+  
+  # cut rows beyond 100 
+  if (dim(df)[2]>100)
+  {
+  df <- df[,1:100] # chop dataframe down 
+  }
+  
+  
+  dataTypes <- vector(mode="character", length=dim(df)[2])  # define a vector to hold each columns data type 
+  # we loop through each column and determine its type 
+  for (i in 1:dim(df)[2])
+  {
+    # first task is to scrub the data 
+    df[,i] <- gsub(" ", "", df[,i]) # remove spaces 
+    df[,i] <- tolower(df[,i])
+    # check to make sure there are no na n/a and we missed this as continuous data 
+    na_indi <- which(df[,i] =="na" | df[,i]=="n/a")
+    if (length(na_indi) > 0 ) # we found some Nas 
+    {
+      df[na_indi,i] <- NA
+    }
+    
+    na_indi <- sum(is.na(df[,i])) # get initial count of na indices 
+    
+    # check if it is numeric by converting to it 
+    test <- df[,i] # holder variable 
+    test <- as.numeric(test) 
+    na_indi2 <- sum(is.na(test))
+    
+    if (na_indi2>na_indi) #must be characters 
+    {
+      dataTypes[i] <- "character"
+      
+    } else 
+    { 
+        dataTypes[i] <- "double"
+        df[,i] <- test
+        
+    }
+  }
+  
+  # we now look to convert to factors 
+
+  for (i in 1:(dim(df)[2]))
+  {
+    if (dataTypes[i] == "character")
+    {
+      dataTypes[i] = "factor"
+      df[,i] <- as.factor(df[,i])
+      if (nlevels(df[,i]) > 6) # bad column and we delete 
+      {
+        # df[,i] <- NULL # remove column 
+        dataTypes[i] <- 0 # mark to remove data type
+      }
+      
+    }
+  }
+  r_indi <- which(dataTypes == 0)
+  df[,r_indi] <- NULL 
+  dataTypes <- dataTypes[-r_indi] 
+  df <- cbind(row_names,df)
+  return(list(dataTypes,df))
+}
+
+input <- DataScrubbing("home/ec2-user/big-dog/public/data/bbqpizza")
+
+input_data <- input[[2]]
+
+row_names <- input_data[,1]
+
+input_data[,1] <- NULL
+
+data <- input_data
+
+# Define UI for application that plots random distributions 
+shinyUI(navbarPage("Big Dog Analytics", id = "tabs",
+  tabPanel("Marginal Distributions", value = "MD",
+  
+  # Sidebar with a slider input for number of observations
+  sidebarPanel(
+	selectInput(inputId = "col_names",
+				label = "Select",
+				colnames(data)), 
+				
+	selectInput(inputId = "show_type",
+				label = "Select",
+				list("Histogram" = "hist", 
+				 "Kernel Density" = "kd", 
+				 "Combined" = "comb")) 
+  ),
+
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("MarginalPlot")
+  )  
+  ),
+  tabPanel("Outlier Analysis", value = "OA",
+	sidebarPanel(
+		sliderInput(inputId = "pval", label = "Rejection P-Value", min=0, max=10, value=5, step = 1)
+	),
+  mainPanel(
+    plotOutput("Outliers")
+  )
+  ),
+  tabPanel("Correlation Analysis", value = "CA",
+	sidebarPanel(),
+  mainPanel(
+    plotOutput("Corr", hover = "plot_hover"
+        ),
+		verbatimTextOutput("hover$info")
+	)
+   ),
+  tabPanel("Mean Vector", value = "MV",
+	sidebarPanel(),
+  mainPanel(
+    plotOutput("Mean_o")
+  )
+  )
+))
 
 shinyServer(function(input, output) {
+  
   Marginals <- function(data,name,type){	
 	if (type == "hist"){
 		p <- ggplot(data, aes_q(x = as.name(name))) + geom_histogram(fill = "deepskyblue2", alpha = 0.2, color = "white") + title("Marginal Distribution") + ylab('Counts')
@@ -96,9 +231,9 @@ shinyServer(function(input, output) {
 	print(p)
   })
   
-  output$click_info <- renderPrint({
-    # With base graphics, need to tell it what the x and y variables are.
-    nearPoints(data, input$plot_click)
-    # nearPoints() also works with hover and dblclick events
+  output$hover_info <- renderPrint({
+    cat("input$plot_hover:\n")
+    str('hi')
   })
 })
+
