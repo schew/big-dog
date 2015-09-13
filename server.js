f45749372e84818a485c836cc51e29d5b168a5de
schew4 <schew4@jhu.edu>
2015-09-12 23:38:30 -0400

    fixed conflict

--- server.js
+++ server.js
@@ -66,26 +66,3 @@ app.listen(port);
 console.log('Magic happens on port ' + port);
 // expose app           
 exports = module.exports = app;
-
-
-// Get R running
-// server.configure(function(){    
-//     server.use(express.static(__dirname + '/public'));
-// });
-// server.get('/', function(req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write('R graph<br>');
-//     process.env.R_WEB_DIR = process.cwd() + '/public';
-//     var child = exec('Rscript script/xyplot.R', function(error, stdout, stderr) {
-//         console.log('stdout: ' + stdout);
-//         console.log('stderr: ' + stderr);
-//         if (error !== null) {
-//             console.log('exec error: ' + error);
-//         }
-//         res.write('<img src="/xyplot.png"/>');
-//         res.end('<br>end of R script');
-//     });
-// });
-
-// server.listen(1337, "127.0.0.1");
-// console.log('Server running at http://127.0.0.1:1337/');
