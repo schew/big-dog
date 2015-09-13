abeb70fef6b88088b76f15ff42536bf548f03219
schew4 <schew4@jhu.edu>
2015-09-13 07:40:50 -0400

    UI

--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -5,26 +5,7 @@ app.controller('DogCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upl
     $scope.uploadFiles = function(file) {
         $scope.f = file;
         if (file && !file.$error) {
-            // file.upload = Upload.upload({
-            //     url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-            //     file: file
-            // });
             file.upload = Upload.upload({
-// <<<<<<< HEAD
-//         		url: 'https://angular-file-upload.s3.amazonaws.com/bigdogdata', //S3 upload url including bucket name
-//         		method: 'POST',
-//         		fields : {
-//           			key: file.name, // the key to store the file on S3, could be file name or customized
-//           			AWSAccessKeyId: <AKIAIBU4ABQ4BHBNDE6A>,
-//           			acl: 'private', // sets the access to the uploaded file in the bucket: private or public
-//           			policy: $scope.policy, // base64-encoded json policy (see article below)
-//           			signature: $scope.signature, // base64-encoded signature based on policy string (see article below)
-//           			"Content-Type": file.type != '' ? file.type : 'application/octet-stream', // content type of the file (NotEmpty)
-//           			filename: file.name // this is needed for Flash polyfill IE8-9
-//         		},
-//         		file: file
-//       		});
-// =======
                  url: 'api/user/uploads',
                   method: 'POST',
                   file: file
