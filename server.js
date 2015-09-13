49ff8ef16710418a722b861a3b3a9b510425e71b
Alec <atab70782@gmail.com>
2015-09-12 23:01:29 -0400

    Added redirect to shiny

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.route('/upload')
 			console.log("Uploading: " + filename);
 	    	var ws = fs.createWriteStream('public/data/' + filename);
 	    	file.pipe(ws);
-	    	
+	    	res.redirect('localhost:'+80);
 		});
 	});
 
