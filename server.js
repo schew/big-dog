4d14b433a7f82cde5f1f36ab85c067b7c71633f5
Alec <atab70782@gmail.com>
2015-09-12 21:55:32 -0400

    bugs

--- server.js
+++ server.js
@@ -9,6 +9,42 @@ var methodOverride = require ('method-override');
 var db = require('./config/database');
 var port = 8080;
 
+// File Upload
+var fs = require('fs-extra');
+var path = require('path');
+var busboy = require('connect-busboy');
+
+// File Upload
+// middleware
+app.use(busboy());
+// for static file storage
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.route('/upload')
+	.post(function (req, res, next) {
+		var fstream;
+		req.pipe(req.busboy);
+		req.busboy.on('file', function(fieldname, file, filename) {
+			console.log("Uploading: " + filename);
+			//Path to where it'll be uploaded
+			fstream = fs.createWriteStream(__dirname + '/data/' + filename);
+
+		    fstream.on('open', function(fd) {
+		    	file.pipe(fstream);
+		    }).on('close', function() {
+				console.log("Upload Finished of " + filename);
+				res.redirect('back');
+			});;
+
+			// file.pipe(fstream);
+			// fstream.on('close', function() {
+			// 	console.log("Upload Finished of " + filename);
+			// 	res.redirect('back');
+			// });
+		});
+	});
+
+
 // connect to our mongoDB database 
 // (uncomment after you enter in your own credentials in config/db.js)
 // mongoose.connect(db.url); 
@@ -28,7 +64,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override')); 
 
 // set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public')); 
+// app.use(express.static(__dirname + '/public')); 
 
 // routes ==================================================
 require('./app/routes')(app); // configure our routes
